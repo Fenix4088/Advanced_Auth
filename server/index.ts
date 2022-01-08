fa6e5dceb18a7840c37d146a75881db273eb7a3d
Fenix4088 <fenix4088@gmail.com>
2022-01-08 15:48:10 +0200

    server: cors config extended

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,10 @@ app.use(express.static('static'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileupload({}));
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}));
 app.use('/api', router);
 
 //! add custom error handler middleware as the last middleware
